065a8c5291f8f1d85ddfc29e2b6ee702ffe4565b
purplebugs <7695311+purplebugs@users.noreply.github.com>
2019-07-05 09:04:54 +0200

    Updated to use versions 7.2.0

--- setup-elastic-kibana.sh
+++ setup-elastic-kibana.sh
@@ -6,11 +6,11 @@
 #TODO Add robustness eg: to fail fast if Docker not installed
 
 # download Elasticsearch & Kibana images
-docker pull docker.elastic.co/elasticsearch/elasticsearch:6.3.2
-docker pull docker.elastic.co/kibana/kibana:6.3.2
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.2.0
+docker pull docker.elastic.co/kibana/kibana:7.2.0
 
-echo "FETCHED docker.elastic.co/elasticsearch/elasticsearch:6.3.2"
-echo "FETCHED docker.elastic.co/kibana/kibana:6.3.2"
+echo "FETCHED docker.elastic.co/elasticsearch/elasticsearch:7.2.0"
+echo "FETCHED docker.elastic.co/kibana/kibana:7.2.0"
 
 # Remove previously installed containers
 docker stop elasticsearch_1
@@ -19,14 +19,14 @@ docker stop kibana_1
 # Run elasticsearch
 echo "Starting elasticsearch_1"
 
-docker run -d --rm --name elasticsearch_1 -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2 && sleep 20s
+docker run -d --rm --name elasticsearch_1 -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.2.0 && sleep 20s
 
 # Takes about 20 secs
 # Or perform GET http://localhost:9200/ and assert on response?
 
 # Run Kibana
 echo "Starting kibana_1"
-docker run -d --rm --name kibana_1 --link elasticsearch_1:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:6.3.2 && sleep 20s
+docker run -d --rm --name kibana_1 --link elasticsearch_1:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:7.2.0 && sleep 20s
 
 echo '--------------- list of running containers ---------------------'
 docker ps
